12b83bb62d6e6a7548bb2b5970d276ae01cae164
neeraj-gs <gsneeraj05@gmail.com>
2023-11-21 20:43:12 +0530

    ADded actions to retrive a specific data and these are the actions preforemd , done with user model adn schema

--- src/db/users.ts
+++ src/db/users.ts
@@ -13,3 +13,16 @@ const UserSchema = new mongoose.Schema({
 
 //model 
 export const UserModel = mongoose.model("User", UserSchema)
+
+export const getUsers =()=> UserModel.find();
+export const getUserByEmail = (email:string) => UserModel.findOne({email})
+export const getUserBySessionToken = (sessionToken:string) => UserModel.findOne({
+    'authentication.sessionToken':sessionToken,
+})
+export const getUserById = (id:String)=> UserModel.findById(id);
+export const createUser = (values:Record<string,any>) =>new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
+
+
+
