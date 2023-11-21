16b2290193483632dd0fec53b0c0ec21286045d2
neeraj-gs <gsneeraj05@gmail.com>
2023-11-21 20:53:04 +0530

    ADded authentication and if no one exits creates a user and make them ad add it to db

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -23,7 +23,7 @@ export const login = async (req: express.Request, res: express.Response) => {
       return res.sendStatus(403);
     }
 
-    const salt = random();
+    const salt = random(); //to createa random string
     user.authentication.sessionToken = authentication(salt, user._id.toString());
 
     await user.save();
@@ -41,18 +41,20 @@ export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body; //extracting data from our body
 
-    if (!email || !password || !username) {
-      return res.sendStatus(400);
+    if (!email || !password || !username) { //if any of the fields are missing we return unauthenticated
+      return res.sendStatus(400).json({msg:"Authentication Failed"});
     }
 
-    const existingUser = await getUserByEmail(email);
+    const existingUser = await getUserByEmail(email); //if email already exirs
+    //this was teh functoin we creted to find a user using email
   
     if (existingUser) {
-      return res.sendStatus(400);
+      return res.sendStatus(400).json({msg:"User Already Exists"});
     }
+    //if no existing user we need otcreae a user anem
 
     const salt = random();
-    const user = await createUser({
+    const user = await createUser({ //create a user
       email,
       username,
       authentication: {
@@ -61,7 +63,7 @@ export const register = async (req: express.Request, res: express.Response) => {
       },
     });
 
-    return res.status(200).json(user).end();
+    return res.status(200).json({"User Created Successfully":user}).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
